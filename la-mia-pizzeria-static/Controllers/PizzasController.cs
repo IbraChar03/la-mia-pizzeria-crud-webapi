16c3125a14c91272f11a0963693b66e0e9e76061
Ibrahim Char <113177312+IbraChar03@users.noreply.github.com>
2023-05-12 14:53:01 +0200

    azione per restituire lista delle pizze

--- la-mia-pizzeria-static/Controllers/PizzasController.cs
+++ la-mia-pizzeria-static/Controllers/PizzasController.cs
@@ -1,11 +1,27 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using la_mia_pizzeria_static.Models;
 
 namespace la_mia_pizzeria_static.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class PizzasController : ControllerBase
     {
+        public IActionResult AllPizzas(string? name)
+        {
+            using(PizzaContext ctx = new PizzaContext())
+            {
+                List<Pizza> pizzas = new List<Pizza>();
+                if(name == null)
+                 pizzas = ctx.Pizzas.ToList();
+                else 
+                 pizzas = ctx.Pizzas.Where(p => p.Name == name).ToList();
+                return Ok(pizzas);
+
+                
+                
+            }
+        }
     }
 }
